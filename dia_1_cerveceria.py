8abf9aec76458bcc74aa2284089dc57bb396dd3b
javips99 <javips99@gmail.com>
2026-07-10 13:53:38 +0200

    Añadidos ejercicios del Día 1

---
+++ dia_1_cerveceria.py
@@ -0,0 +1,7 @@
+ #print ("El nombre para la nueva cerveza es '",
+ #  input("Introduce el nombre de un objeto "), input("Introduce un color "), "'")
+
+# Solución del curso
+print("El nombre de tu cervecería\nes "
+      + input("¿Qué ciudad te gustaría visitar?: ") + " "
+      + input("¿Cuál es tu color favorito?: ") + "\nFelicitaciones!!!!")
---
+++ dia_1_ejemplos_teoricos.py
@@ -0,0 +1,28 @@
+
+'''
+print("Hola Mundo")
+print(5 + 3)
+
+'''
+#print(input("Escribe tu nombre:") + input("Escribe tu apellido:"))
+
+#print("Hola, " + (input("¿como te llamas?") + "!!!" ))
+
+#print((int(input("Número: "))) + int(input("Otro número: ")))
+
+#print(float(input("introduce el precio")) * 1.16)
+
+'''
+print("=====CALCULADORA=====")
+print("Resultado: ", float(input("Introduce el primer número: ")) + float(input("Introduce el segundo número: ")))
+'''
+
+#print("Tu edad es: ", 2025 - int(input("¿En que año naciste? ")),  "años")
+
+'''
+print("=====AREA DEL RECTÁNGULO=====")
+print("El area del rectángulo es: ", int(input("Base: ")) * int(input("Altura: ")))
+'''
+
+#print("Hola, " + input("¿Cual es tu nombre? ") + " tu eres de " + input("Escribe tu ciudad "))
+print("220" + "35")
