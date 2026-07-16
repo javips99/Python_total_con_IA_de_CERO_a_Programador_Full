95b1aa1b38bd13443568c48d32f2f3ee6334793d
javips99 <javips99@gmail.com>
2026-07-16 18:19:09 +0200

    Añadir al arcivo el ulimo ejercicio

--- dia_5_ejemplos_teoricos/interaccion_funciones.py
+++ dia_5_ejemplos_teoricos/interaccion_funciones.py
@@ -118,3 +118,36 @@ resultado_promedio = promedio(lista_limpia)
 
 print(f"La lista reducida es: {lista_limpia}")
 print(f"El promedio de la lista es: {resultado_promedio}")
+
+'''
+Crea una función (llamada lanzar_moneda) que devuelva el resultado de lanzar una moneda (al azar). 
+Dicha función debe poder devolver los resultados "Cara" o "Cruz", y no debe recibir argumentos para funcionar.
+Crea una segunda función (llamada probar_suerte), que tome dos argumentos: 
+    -El primero, debe ser el resultado del lanzamiento de la moneda. 
+    -El segundo argumento, será una lista de números cualquiera (debes crear una lista con valores y llamarla lista_numeros).
+Si se le proporciona una "Cara", debe mostrar el mensaje al usuario: "La lista se autodestruirá", 
+y eliminarla (devolverla como lista vacía []).
+Si se le proporciona una "Cruz", debe imprimir en pantalla: "La lista fue salvada" y devolver la lista intacta.
+Pistas: utiliza el método choice de la biblioteca random para elegir un elemento al azar de una secuencia.
+
+'''
+from random import choice
+
+lista_numeros = [ 5, 27, 6, 78, 95, 2]
+
+def lanzar_moneda():
+    moneda = choice(["Cara", "Cruz"])
+    return moneda
+
+
+def probar_suerte(resultado, lista_numeros):
+    if resultado == "Cara":
+        print("La lista se autodestruirá")
+        return []
+    else:
+        print("La lista fue salvada")
+        return lista_numeros
+    
+resultado_lanzamiento = lanzar_moneda()
+lista_final = probar_suerte(resultado_lanzamiento, lista_numeros)
+print(lista_final)
