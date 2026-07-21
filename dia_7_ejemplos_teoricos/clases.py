3f0416d543ba97030d9439940e2d3ba32b710e92
Javips99 <javips99@gmail.com>
2026-07-21 17:25:20 +0200

    Ejercicios de clases

---
+++ dia_7_ejemplos_teoricos/clases.py
@@ -0,0 +1,46 @@
+
+class Pajaro:
+    pass
+
+mi_pajaro = Pajaro()
+print(type(mi_pajaro))  # <class '__main__.Pajaro'>
+
+otro_pajaro = Pajaro()
+print(type(otro_pajaro))  
+
+# Ejercicios
+
+'''
+Crea una clase llamada Personaje y a continuación, 
+crea un objeto a partir de ella, por ejemplo: harry_potter
+
+''' 
+class Personaje:
+    pass
+
+harry_potter = Personaje()
+
+'''
+Crea una clase llamada Dinosaurio, 
+y tres instancias a partir de ella: velociraptor, tiranosaurio_rex y braquiosaurio
+
+'''
+class Dinosaurio:
+    pass
+
+velociraptor = Dinosaurio()
+tiranosaurio_rex = Dinosaurio()
+braquiosaurio = Dinosaurio()
+
+'''
+Crea una clase llamada PlataformaStreaming 
+y crea los siguientes objetos a partir de ella: netflix, hbo_max, amazon_prime_video
+
+'''
+class PlataformaStreaming:
+    pass
+
+netflix = PlataformaStreaming()
+hbo_max = PlataformaStreaming()
+amazon_prime_video = PlataformaStreaming()
+
