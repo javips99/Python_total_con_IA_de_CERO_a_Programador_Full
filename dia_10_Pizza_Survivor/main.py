ca73247333d390b28b1b32067c55de0e7a01a184
Javips99 <javips99@gmail.com>
2026-07-24 12:07:19 +0200

    Cargar imagen del repartidor, configurar posición inicial y aplicar función de dibujo en el bucle principal

--- dia_10_Pizza_Survivor/main.py
+++ dia_10_Pizza_Survivor/main.py
@@ -16,6 +16,16 @@ fondo = pygame.image.load("fondo.png")  # Cargar la imagen de fondo
 fondo = pygame.transform.scale(fondo, (800, 600))  # Escalar la imagen de fondo al tamaño de la ventana
 
 
+# Repartidor de pizzas
+repartidor_img = pygame.image.load("repartidor.png")  # Cargar la imagen del repartidor
+repartidor_img = pygame.transform.scale(repartidor_img, (64, 100)) # Tamaño del repartidor
+repartidor_x = 368  # Posición inicial en el eje X
+repartidor_y = 440  # Posición inicial en el eje Y
+
+def repartidor(x, y):
+    pantalla.blit(repartidor_img, (x, y))  # Dibujar el repartidor en la pantalla
+
+
 # Loop del juego
 se_ejecuta = True
 
@@ -27,7 +37,7 @@ while se_ejecuta:
             se_ejecuta = False
 
     pantalla.blit(fondo, (0, 0))  # Dibujar el fondo en la pantalla
-
+    repartidor(repartidor_x, repartidor_y)  # Dibujar el repartidor en la pantalla
     # Actualizar la pantalla
     pygame.display.update() 
 
